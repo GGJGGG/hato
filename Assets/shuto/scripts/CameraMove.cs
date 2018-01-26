c00aed72c49ab62774a540105f586636e600539f
ShutoAoyagi <m01150045a@edu.teu.ac.jp>
2018-01-27 04:18:15 +0900

    [追加]はとの動きと電波のパーティクルとカメラの動きを作成

---
+++ Assets/shuto/scripts/CameraMove.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMove : MonoBehaviour {
+
+    private GameObject player;
+    private Vector3 offset = Vector3.zero;
+
+	// Use this for initialization
+	void Start () {
+        player = GameObject.FindGameObjectWithTag("Player");
+        offset = transform.position - player.transform.position;
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+        Vector3 newPosition = transform.position;
+        newPosition.x = player.transform.position.x + offset.x;
+        newPosition.z = player.transform.position.z + offset.z;
+        transform.position = newPosition;
+    }
+}
---
+++ Assets/shuto/scripts/Hato.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hato : MonoBehaviour {
+
+    [SerializeField]
+    private float speed = 10;
+    [SerializeField]
+    private Vector3 hatoVelocity;
+
+    // Use this for initialization
+    void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        HatoMove();
+    }
+
+    void HatoMove()
+    {
+        hatoVelocity.x = 3;
+        hatoVelocity.y -= 0.02f;
+        if (Input.GetKey(KeyCode.W))
+        {
+            hatoVelocity.y += speed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            hatoVelocity.y -= speed * Time.deltaTime;
+        }
+
+        GetComponent<Rigidbody>().velocity = hatoVelocity;
+    }
+
+}
