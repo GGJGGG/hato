ac6d008dd78eafb2d19966e7eb347585eb74f0b2
fumi <fumilin01@gmail.com>
2018-01-27 10:07:32 +0900

    目的地に到達できるようにした

---
+++ Assets/Fumi/Scripts/Hato/Bomb.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bomb : MonoBehaviour
+{
+    [SerializeField] ParticleSystem clearEffect;
+
+    // ジョイントから切り離す
+    public void Detach()
+    {
+        var joint = GetComponent<FixedJoint>();
+        Destroy(joint);
+        var col = GetComponent<Collider>();
+        col.enabled = false;
+    }
+
+    public void PlayClearEffect(float rate = 1.0f)
+    {
+        var particleNum = (int)(100 * rate);
+        clearEffect.Emit(particleNum);
+    }
+}
