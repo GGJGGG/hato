6aacda5c359f0374198219878703413db6ceb953
ShutoAoyagi <m01150045a@edu.teu.ac.jp>
2018-01-28 01:37:16 +0900

    [追加]プレイヤーの羽ばたきと気絶アニメーションを設定しました

---
+++ Assets/shuto/scripts/PlayerAnim.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnim : MonoBehaviour {
+
+    Animator _animator;
+
+    // Use this for initialization
+    void Start () {
+        _animator = GetComponent<Animator>();
+	}
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            _animator.SetBool("faint", true);
+        }
+        if (Input.GetKeyDown(KeyCode.G))
+        {
+            _animator.SetBool("faint", false);
+        }
+    }
+}
