ebefa9e8883ce272a58e87c71bf6a25a10c39ab4
fumi <fumilin01@gmail.com>
2018-01-28 08:08:56 +0900

    ゲームオーバー時にGameの最初に戻る

---
+++ Assets/Fumi/Scripts/Hato/GameLoop.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.SceneManagement;
+
+
+// ゲームのループに関わるイベントを記述する予定
+public class GameLoop : MonoBehaviour
+{
+    [SerializeField] float onPlayerDeadWaitSec = 2.0f;
+
+    void OnEnable()
+    {
+        GameEventManager.Instance.OnPlayerDead += OnPlayerDead;
+    }
+
+    void OnDisable()
+    {
+        GameEventManager.Instance.OnPlayerDead -= OnPlayerDead;
+    }
+
+    // ゲームオーバーからGameやり直し。演出込み
+    void OnPlayerDead()
+    {
+        StartCoroutine(PlayerDeadProgress());
+    }
+
+    IEnumerator PlayerDeadProgress()
+    {
+        yield return new WaitForSeconds(onPlayerDeadWaitSec);
+        SceneManager.LoadScene("Game");
+    }
+}
