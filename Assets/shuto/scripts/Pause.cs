b462d91f59abdaa93750d54c782f0076cf9c59c7
ShutoAoyagi <m01150045a@edu.teu.ac.jp>
2018-01-27 19:20:50 +0900

    [変更]ポーズ機能を記述するスクリプトを変更

--- Assets/shuto/scripts/Pause.cs
+++ Assets/shuto/scripts/Pause.cs
@@ -6,11 +6,11 @@ public class Pause : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
+        Time.timeScale = 0.0f;
+    }
+
+    // Update is called once per frame
+    void Update () {
         if (Input.GetKeyDown(KeyCode.P))
         {
             Time.timeScale = 0.0f;
