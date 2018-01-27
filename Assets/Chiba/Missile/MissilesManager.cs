187c5f76bcee6b373b22254eb9c235c3a85da99a
Chiba Isami <isami620@gmail.com>
2018-01-27 13:27:50 +0900

    ミサイルの追加

---
+++ Assets/Chiba/Missile/MissilesManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MissilesManager : MonoBehaviour
+{
+    public GameObject Missle;
+
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    { 
+    }
+
+      void OnBecameVisible()
+
+      {
+        Missle.GetComponent<MissilesMove>().MisslesStart();
+
+      }
+
+}
