3ac18c2df4607f86885c7cead9ac130e240cc608
ShutoAoyagi <m01150045a@edu.teu.ac.jp>
2018-01-27 18:47:22 +0900

    [追加]爆発のエフェクトとポーズ機能の作成

---
+++ Assets/shuto/scripts/Pause.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pause : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            Time.timeScale = 0.0f;
+        }
+        if (Input.GetKeyDown(KeyCode.O))
+        {
+            Time.timeScale = 1.0f;
+        }
+    }
+}
