fe9231ddb9629310520c375ebf05a869efa94b12
fumi <fumilin01@gmail.com>
2018-01-28 00:27:10 +0900

    音程を表すUIを組み込んだ

--- Assets/Fumi/Scripts/Hato/InputPowerView.cs
+++ Assets/Fumi/Scripts/Hato/InputPowerView.cs
@@ -5,10 +5,18 @@ using UnityEngine.UI;
 
 public class InputPowerView : MonoBehaviour
 {
-    [SerializeField] Image fillImage;
+    [SerializeField] RectTransform rt;
+    [SerializeField] Button button;
+    [SerializeField] Text text;
 
-    public void SetPower(float rate)
+    public void SetPower(float rate, int freq)
     {
-        fillImage.fillAmount = rate;
+        var noInput = rate == 0.0f;
+        var anchor = new Vector2(0.5f, noInput ? 0.5f : rate);
+        rt.anchorMin = anchor;
+        rt.anchorMax = anchor;
+        rt.anchoredPosition = new Vector3(8f, 0);
+        button.interactable = !noInput;
+        text.text = string.Format("{0}Hz", freq);
     }
 }
