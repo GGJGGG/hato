a176a226ae21433b1ae027c53b3f70de4bedbcda
ShutoAoyagi <m01150045a@edu.teu.ac.jp>
2018-01-28 14:52:25 +0900

    [追加]ゲームオーバー画面の制作

---
+++ Assets/shuto/scripts/GameOverDisplay.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.SceneManagement;
+
+
+public class GameOverDisplay : MonoBehaviour {
+
+    [SerializeField] GameObject gameOver;
+    [SerializeField] GameObject blackBack;
+
+    private void Start()
+    {
+        gameOver.SetActive(false);
+        blackBack.SetActive(false);
+    }
+
+    void OnEnable()
+    {
+        if (GameEventManager.Instance != null)
+        {
+            GameEventManager.Instance.OnPlayerDead += OnPlayerDead;
+        }
+    }
+
+    void OnDisable()
+    {
+        if (GameEventManager.Instance != null)
+        {
+            GameEventManager.Instance.OnPlayerDead -= OnPlayerDead;
+        }
+    }
+
+    void OnPlayerDead()
+    {
+        gameOver.SetActive(true);
+        blackBack.SetActive(true);
+    }
+}
