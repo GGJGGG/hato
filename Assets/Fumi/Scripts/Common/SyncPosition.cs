5295b25a9bf19d18f1afeb8e1cafe07db501ee93
fumi <fumilin01@gmail.com>
2018-01-28 13:25:08 +0900

    クリア時、バームクーヘンをズーム

--- Assets/Fumi/Scripts/Common/SyncPosition.cs
+++ Assets/Fumi/Scripts/Common/SyncPosition.cs
@@ -6,6 +6,7 @@ public class SyncPosition : MonoBehaviour
     public Transform _targetTransform;
     public Vector3 AddPosition;
     public Vector3 MulPosition;
+    public float EffectiveRate = 1.0f;
 	Transform _trans;
 
 	void Start()
@@ -17,6 +18,8 @@ public class SyncPosition : MonoBehaviour
 	{
 		if(_targetTransform == null) return;
 
-        _trans.position = Vector3.Scale(MulPosition,_targetTransform.position)  + AddPosition;
+        var newPos = Vector3.Scale(MulPosition,_targetTransform.position)  + AddPosition;
+
+        _trans.position = Vector3.Lerp(_trans.position, newPos, EffectiveRate);
 	}
 }
