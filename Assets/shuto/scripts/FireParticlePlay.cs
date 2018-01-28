8b1976b933c85358cf2883affbcec9d9c0bee155
ShutoAoyagi <m01150045a@edu.teu.ac.jp>
2018-01-28 12:45:17 +0900

    [追加]爆発のパーティクルをミサイルの方に出るように設定しました。

---
+++ Assets/shuto/scripts/FireParticlePlay.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireParticlePlay : MonoBehaviour {
+
+    [SerializeField] ParticleSystem particle;
+
+    void OnCollisionEnter(GameObject other)
+    {
+        Debug.Log("hithit");
+        if (other.tag == "Player")
+        {
+            particle.Play(true);
+        }
+    }
+}
